df0ef720e87a5f473efa862412bafd4e0f3ecb41
sandenson <lgabrielsandes@gmail.com>
2026-03-31 19:55:36 -0300

    feat: Edit and delete buttons added to the Table component.

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,10 +1,12 @@
-import { ITransaction } from "@/types/transaction"
-import { formatDate, formatPrice } from "@/utils"
+import { ITransaction } from "@/types/transaction";
+import { formatDate, formatPrice } from "@/utils";
+import Image from "next/image";
 
 export type TableProps = {
-    data: ITransaction[]
+    data: ITransaction[];
+    handleOpenFormModal: (transaction: ITransaction) => void;
 }
-export const Table = ({ data }: TableProps) => {
+export const Table = ({ data, handleOpenFormModal }: TableProps) => {
     return <>
         <table className="w-full mt-16 border-separate border-spacing-y-2">
             <thead>
@@ -12,7 +14,9 @@ export const Table = ({ data }: TableProps) => {
                    <th className="px-4 text-left text-table-header text-base font-medium">Título</th> 
                    <th className="px-4 text-left text-table-header text-base font-medium">Preço</th> 
                    <th className="px-4 text-left text-table-header text-base font-medium">Categoria</th> 
-                   <th className="px-4 text-left text-table-header text-base font-medium">Data</th> 
+                   <th className="px-4 text-left text-table-header text-base font-medium">Data</th>
+                   <th className="px-4 text-left text-table-header text-base font-medium">Editar</th>
+                   <th className="px-4 text-left text-table-header text-base font-medium">Excluir</th>
                 </tr>
             </thead> 
             <tbody>
@@ -21,7 +25,17 @@ export const Table = ({ data }: TableProps) => {
                    <td className="px-4 py-4 whitespace-nowrap text-title bg-white rounded-l-lg">{transaction.title} </td> 
                    <td className={`px-4 py-4 whitespace-nowrap ${transaction.type === "INCOME"? "text-income": "text-outcome"} bg-white text-right`}>{formatPrice(transaction.price)} </td> 
                    <td className="px-4 py-4 whitespace-nowrap text-title bg-white">{transaction.category} </td>
-                   <td className="px-4 py-4 whitespace-nowrap text-title bg-white rounded-r-lg">{formatDate(transaction.data)} </td>
+                   <td className="px-4 py-4 whitespace-nowrap text-title bg-white">{formatDate(transaction.data)} </td>
+                   <td className="px-4 py-4 whitespace-nowrap text-title bg-white">
+                    <button type="button" className="cursor-pointer" onClick={() => handleOpenFormModal(transaction)}>
+                        <Image src={'/images/edit.png'} alt={`edit icon`} width={24} height={24} />
+                    </button>
+                   </td>
+                   <td className="px-4 py-4 whitespace-nowrap text-title bg-white rounded-r-lg">
+                    <button type="button" className="cursor-pointer">
+                        <Image src={'/images/delete.png'} alt={`delete icon`} width={24} height={24} />
+                    </button>
+                   </td>
                 </tr>
                ))} 
             </tbody>
